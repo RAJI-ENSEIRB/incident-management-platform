52e3ef53b6bdfb2493240d7c815ffd7a45b5c918
RAJI-ENSEIRB <oussama.raji@enseirb-matmeca.fr>
2026-03-10 15:07:23 +0100

    init backend skeleton with local postgres

---
+++ src/test/java/com/amine/incidentbackend/IncidentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.incidentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
